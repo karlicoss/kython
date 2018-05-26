44a046d200b7cbf32810b935bc5f8b080db82422
Dima Gerasimov <karlicoss@gmail.com>
2018-05-26 14:08:29 +0200

    tools to work with gsettings

--- kython/config.py
+++ kython/config.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 def trigger_run(cmd: str):
     """
        trigger program run, for instance, to create config
@@ -16,3 +18,51 @@ def patch_py(path, property, new_value):
     print(cmd)
     import subprocess
     subprocess.check_call(' '.join(['sed', '-i', cmd, path]), shell=True)
+
+
+def gsettings_encode(value: Any):
+    if type(value) == list:
+        encoded = [gsettings_encode(i) for i in value]
+        return "[" + ', '.join(encoded) + "]"
+    if type(value) == tuple:
+        encoded = [gsettings_encode(i) for i in value]
+        return "(" + ', '.join(encoded) + ")"
+    elif type(value) == str:
+        return "'{}'".format(value)
+    elif type(value) == int:
+        return f"{value}"
+    elif type(value) == bool:
+        return "true" if value else "false"
+    else:
+        raise RuntimeError("Unexpected type: {}".format(type(value)))
+
+# TODO eh, that doesnt really work, what is more, read doesnt always work
+ # dconf read -d /org/mate/power-manager/button-lid-ac
+def get_default(key: str):
+    from subprocess import check_output
+    return check_output(['dconf', 'read', '-d', key]).decode('utf-8')
+
+def set_dconf(key: str, value: Any, check_schema=True):
+    # TODO ugh
+    from subprocess import call, DEVNULL, check_call
+    if "/" in key:
+        sp = key.split('/')
+        assert sp[0] == ''
+        del sp[0]
+        kname = sp[-1]
+        gschema = '.'.join(sp[:-1])
+    if check_schema:
+        code = call(['gsettings', 'describe', gschema, kname], stdout=DEVNULL)
+        if code != 0:
+            raise RuntimeError(f"{gschema}:{kname} doesn't exist!")
+
+    # TODO use gsettings set for that??
+    command = ['dconf', 'write', key, gsettings_encode(value)]
+    print(command)
+    check_call(command)
+
+def set_gsettings(schema: str, key: str, value: Any):
+    from subprocess import check_call
+    command = ['gsettings', 'set', schema, key, gsettings_encode(value)]
+    print(command)
+    check_call(command)
