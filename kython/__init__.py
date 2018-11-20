b69bd7c830ad430cc98467f78b75bdd0468caed7
Dima Gerasimov <karlicoss@gmail.com>
2018-11-20 08:50:22 +0000

    add preamble

--- kython/__init__.py
+++ kython/__init__.py
@@ -1,1 +1,6 @@
 from .misc import *
+
+def json_loadf(fname: str):
+    import json
+    with open(fname, 'r') as fo:
+        return json.load(fo)
---
+++ kython/preamble.py
@@ -0,0 +1,3 @@
+from os.path import lexists, join, abspath, basename
+from os import listdir
+import json
