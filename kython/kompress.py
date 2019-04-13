e9fc7fc63b66855a7e7cb4bd7625ad97ae762490
Dima Gerasimov <karlicoss@gmail.com>
2019-04-13 14:58:05 +0100

    handle lz4

--- kython/kompress.py
+++ kython/kompress.py
@@ -12,6 +12,9 @@ def open(path: PathIsh, *args, **kwargs): # TODO is it bytes stream??
         return lzma.open(pp, *args, **kwargs)
     elif suf in ('.zip',):
         return ZipFile(pp).open(*args, **kwargs)
+    elif suf in ('.lz4',):
+        import lz4.frame # type: ignore
+        return lz4.frame.open(str(pp))
     else:
         return pp.open(*args, **kwargs)
 
