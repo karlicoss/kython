40ccb607cbcf9ec17f1bb0e986badfba2e1aca09
Dima Gerasimov <karlicoss@gmail.com>
2019-05-03 22:38:57 +0100

    more elaborate konsume module

--- kython/konsume.py
+++ kython/konsume.py
@@ -19,3 +19,149 @@ def zoom(dd, *keys):
         return vals[0] # eh, not sure...
     else:
         return vals
+
+
+def dict_wrap(d):
+    pass
+
+
+    if isinstance(j, dict):
+        pass
+    pass
+
+class Wrapper:
+    pass
+
+    # def consumed():
+    #     # TODO start with dicts??
+    #     # TODO unwrap method or value property?
+    #     # TODO if we use value, it's consumed?
+    #     pass
+from typing import Any
+
+class Zoomable:
+    def __init__(self, parent, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.parent = parent
+
+    def zoom(self):
+        assert self.parent is not None
+        self.parent._remove(self)
+        return self
+
+    def _remove(self, xx):
+        raise NotImplementedError
+
+    def iter_nonempty(self):
+        raise NotImplementedError
+
+    def this_consumed(self):
+        raise NotImplementedError
+
+    @property
+    def nonempty(self):
+        return list(self.iter_nonempty())
+
+from collections import OrderedDict
+class Wdict(Zoomable, OrderedDict):
+    def iter_nonempty(self):
+        for k, v in self.items():
+            yield from v.iter_nonempty()
+        if len(self) > 0:
+            yield self
+
+    def _remove(self, xx):
+        keys = [k for k, v in self.items() if v is xx]
+        assert len(keys) == 1
+        del self[keys[0]]
+
+    def this_consumed(self):
+        return len(self) == 0
+
+    def consumed(self):
+        return len(self.nonempty) == 0
+
+class Wint(Zoomable):
+    def __init__(self, parent, value: int) -> None:
+        super().__init__(parent)
+        self.value = value
+
+    def this_consumed(self):
+        return True # TODO not sure..
+
+    def iter_nonempty(self):
+        yield self
+        # TODO shit inheritance doesn't work..
+# class Wint(Zoomable, int):
+#     def __new__(cls, *args, **kwargs):
+#         print(cls)
+#         print(args)
+#         print(kwargs)
+#         return super().__new__(cls, 5)
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__()
+    #     print("HI")
+
+def _wrap(j, parent=None):
+    # TODO the very top level is kinda special, it needs to assert that all the children are consumed...
+    if isinstance(j, dict):
+        # TODO need to make wdict first, then populate it with values? wonder if should inherit from ordered dict??
+        res = Wdict(parent)
+        cc = [res]
+        for k, v in j.items():
+            vv, c  = _wrap(v, parent=res)
+            res[k] = vv
+            cc.extend(c)
+        return res, cc
+    if isinstance(j, int):
+        res = Wint(parent, j)
+        return res, [res]
+    raise RuntimeError(str(j))
+
+from contextlib import contextmanager
+
+class UnconsumedError(Exception):
+    pass
+
+@contextmanager
+def wrap(j):
+    w, children = _wrap(j)
+
+    yield w
+
+    # from pprint import pprint
+    # pprint(children)
+    for c in children:
+        if not c.this_consumed(): # TODO hmm. how does it figure out if it's consumed???
+            raise UnconsumedError(str(c))
+
+def test_unconsumed():
+    import pytest # type: ignore
+    with pytest.raises(UnconsumedError):
+        with wrap({'a': 1234}) as w:
+            pass
+
+    with pytest.raises(UnconsumedError):
+        with wrap({'c': {'d': 2222}}) as w:
+            d = w['c']['d'].zoom()
+
+def test_consumed():
+    with wrap({'a': 1234}) as w:
+        a = w['a'].zoom()
+
+    with wrap({'c': {'d': 2222}}) as w:
+        c = w['c'].zoom()
+        d = c['d'].zoom()
+
+# def test():
+#     with wrap({'a': {'xx': 123}, 'b': 222}) as w:
+#         # w, a, b, xx
+#         # TODO not sure about nonempty...
+#         # assert len(w.nonempty) == 4
+#         a = w['a'].zoom()
+#         b = w['b'].zoom()
+#         # TODO b obj is not consumed yet
+#         # TODO nonempty method?
+#         print(w.nonempty)
+#         pass
+#     # TODO
