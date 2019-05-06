a7d4033f81e58a6dd7581be91c1d2a3bc4c93fbd
Dima Gerasimov <karlicoss@gmail.com>
2019-05-06 13:30:09 +0100

    split_by

--- kython/kjq.py
+++ kython/kjq.py
@@ -4,10 +4,10 @@ def pipe(*queries):
 def jdel(q):
     return f'del({q})'
 
-def jq_del_all(*keys):
+def jq_del_all(*keys, split_by=10):
     parts = []
     # TODO shit. looks like query might be too long for jq...
-    for q in range(0, len(keys), 10):
-        kk = keys[q: q + 10]
+    for q in range(0, len(keys), split_by):
+        kk = keys[q: q + split_by]
         parts.append(jdel('.. | ({})'.format(', '.join('.' + k + '?' for k in kk))))
     return pipe(*parts)
