baf797095116f181b4b612b6eca9761ab6940167
Dima Gerasimov <karlicoss@gmail.com>
2019-05-19 10:32:45 +0100

    simplify, handle newer takeouts

--- kython/ktakeout.py
+++ kython/ktakeout.py
@@ -3,25 +3,24 @@ import re
 from pathlib import Path
 from datetime import datetime
 from html.parser import HTMLParser
-from typing import List, Dict
+from typing import List, Dict, Optional
 from collections import OrderedDict
 from urllib.parse import unquote
 import pytz
 
-# TODO wonder if that old format used to be UTC...
 # Mar 8, 2018, 5:14:40 PM
 _TIME_FORMAT = "%b %d, %Y, %I:%M:%S %p"
-# TODO %Z?
 
 # ugh. something is seriously wrong with datetime, it wouldn't parse timezone aware UTC timestamp :(
 def parse_dt(s: str) -> datetime:
-    # dt = parser.parse(s)
-    dt = datetime.strptime(s, _TIME_FORMAT)
+    fmt = _TIME_FORMAT
+    if s.endswith('UTC'): # old takeouts didn't have timezone
+        fmt += ' %Z'
+    dt = datetime.strptime(s, fmt)
     if dt.tzinfo is None:
         # TODO log?
         # hopefully it was utc? Legacy, so no that much of an issue anymore..
-        # TODO DO NOT USE IT!!
-        dt = dt.replace(tzinfo=pytz.utc)
+        dt = pytz.utc.localize(dt)
     return dt
 
 
@@ -31,23 +30,15 @@ class State(Enum):
     PARSING_LINK = 2
     PARSING_DATE = 3
 
+
+
 # would be easier to use beautiful soup, but ends up in a big memory footprint..
 class TakeoutHTMLParser(HTMLParser):
     def __init__(self, callback) -> None:
         super().__init__()
         self.state: State = State.OUTSIDE
-        self.current: Dict[str, str] = {}
+        self.url: Optional[str] = None
         self.callback = callback
-
-    def _reg(self, name, value):
-        assert name not in self.current
-        self.current[name] = value
-
-    def _astate(self, s): assert self.state == s
-
-    def _trans(self, f, t):
-        self._astate(f)
-        self.state = t
 
     # enter content cell -> scan link -> scan date -> finish till next content cell
     def handle_starttag(self, tag, attrs):
@@ -62,12 +53,7 @@ class TakeoutHTMLParser(HTMLParser):
             if hr.startswith(prefix + "http"):
                 hr = hr[len(prefix):]
                 hr = unquote(hr) # TODO not sure about that...
-            # TODO should just set in PARSING_LINK?
-            self._reg('url', hr)
-
-    def handle_endtag(self, tag):
-        if tag == 'html':
-            pass # ??
+            assert self.url is None; self.url = hr
 
     # youtube example:
     # Watched Jamie xx - Gosh
@@ -75,59 +61,58 @@ class TakeoutHTMLParser(HTMLParser):
     # Jun 21, 2018, 5:48:34 AM
     # Products:
     #  YouTube
-
     def handle_data(self, data):
         if self.state == State.OUTSIDE:
-            # TODO FIXME
-            prefix = "Watched" # "Visited"
-            if data[:-1].strip() == prefix:
+            if data[:-1].strip() in ("Watched", "Visited"):
                 self.state = State.INSIDE
                 return
 
         if self.state == State.PARSING_LINK:
-            # self._reg(Entry.link, data)
             self.state = State.PARSING_DATE
             return
 
         # TODO extracting channel as part of wereyouhere could be useful as well
         # need to check for regex because there might be some stuff in between
         if self.state == State.PARSING_DATE and re.search(r'\d{4}.*:.*:', data):
-            self._reg('time', data.strip()) # TODO why am I doingthat?
-
-            url = self.current['url']
-            times = self.current['time']
-            time = parse_dt(times)
+            time = parse_dt(data.strip())
             assert time.tzinfo is not None
 
-            self.callback(time, url)
+            assert self.url is not None
+            self.callback(time, self.url)
+            self.url = None
 
-            self.current = {}
             self.state = State.OUTSIDE
             return
 
-def _helper(path):
+def _helper(archive, path):
     from kython import kompress
     collected = []
     def callback(dt, url):
         collected.append((dt, url))
 
     p = TakeoutHTMLParser(callback)
-    with kompress.open('/L/backups/takeout/karlicoss_gmail_com/takeout-20180623T190546Z-001.zip', path) as fo:
+    with kompress.open(archive, path) as fo:
         data = fo.read().decode('utf8')
         print("parsing...")
         p.feed(data) # TODO would be nice to do it in iterative fashion... not sure if python lib supports that
 
     return collected
 
+ARCHIVES = [
+    '/L/backups/takeout/karlicoss_gmail_com/takeout-20180623T190546Z-001.zip',
+    '/L/backups/takeout/karlicoss_gmail_com/takeout-20190507T062236Z-001.zip',
+]
 
 def test_myactivity():
-    collected = _helper('Takeout/My Activity/Chrome/MyActivity.html')
-    assert len(collected) > 100
-    # TODO test timestamp
+    for a in ARCHIVES:
+        collected = _helper(a, 'Takeout/My Activity/Chrome/MyActivity.html')
+        assert len(collected) > 100
+        # TODO test timestamp
 
 def test_youtube():
-    collected = _helper('Takeout/My Activity/YouTube/MyActivity.html')
-    assert len(collected) > 100
-    assert (datetime(year=2018, month=6, day=21, hour=5, minute=48, second=34, tzinfo=pytz.utc), 'https://www.youtube.com/watch?v=hTGJfRPLe08') in collected
+    for a in ARCHIVES:
+        collected = _helper(a, 'Takeout/My Activity/YouTube/MyActivity.html')
+        assert len(collected) > 100
+        assert (datetime(year=2018, month=6, day=21, hour=5, minute=48, second=34, tzinfo=pytz.utc), 'https://www.youtube.com/watch?v=hTGJfRPLe08') in collected
 
 
