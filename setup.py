53264f2d54beafe2731dfa34f429cb3df61fcb1b
Dima Gerasimov <karlicoss@gmail.com>
2019-07-02 22:01:49 +0100

    basic setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+if __name__ == '__main__':
+    setup(name='kython',
+          version='0.1',
+          description='My collection of Python utilities',
+          url='http://github.com/karlicoss/kython',
+          author='Dmitrii Gerasimov',
+          author_email='karlicoss@gmail.com',
+          license='MIT',
+          packages=['kython'],
+          zip_safe=False)
