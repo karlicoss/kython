6cb66e6863d8d0a1e1c6c0c5e556f60a756f2ba3
Dima Gerasimov <karlicoss@gmail.com>
2019-07-23 09:22:39 +0100

    use doublewrap for timed decorator

--- kython/misc.py
+++ kython/misc.py
@@ -513,10 +513,30 @@ def import_file(p: PathIsh, name=None):
     return foo
 
 
+# https://stackoverflow.com/questions/653368/how-to-create-a-python-decorator-that-can-be-used-either-with-or-without-paramet
+def doublewrap(f):
+    '''
+    a decorator decorator, allowing the decorator to be used as:
+    @decorator(with, arguments, and=kwargs)
+    or
+    @decorator
+    '''
+    @wraps(f)
+    def new_dec(*args, **kwargs):
+        if len(args) == 1 and len(kwargs) == 0 and callable(args[0]):
+            # actual decorated function
+            return f(args[0])
+        else:
+            # decorator arguments
+            return lambda realf: f(realf, *args, **kwargs)
+
+    return new_dec
+
 from functools import wraps
 import time
 import logging
 
+@doublewrap
 def timed(func, logger=None):
     if logger is None:
         logger = logging
