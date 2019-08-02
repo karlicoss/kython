c9e1c3c1e4f6d00c5a5dbc66b1b311ec02477758
Dima Gerasimov <karlicoss@gmail.com>
2019-08-02 15:22:15 +0100

    add py.typed

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 from setuptools import setup # type: ignore
 
 if __name__ == '__main__':
-    setup(name='kython',
-          version='0.1',
-          description='My collection of Python utilities',
-          url='http://github.com/karlicoss/kython',
-          author='Dmitrii Gerasimov',
-          author_email='karlicoss@gmail.com',
-          license='MIT',
-          packages=['kython'],
-          zip_safe=False)
+    setup(
+        name='kython',
+        version='0.1',
+        description='My collection of Python utilities',
+        url='http://github.com/karlicoss/kython',
+        author='Dmitrii Gerasimov',
+        author_email='karlicoss@gmail.com',
+        license='MIT',
+        packages=['kython'],
+        zip_safe=False,
+        package_data={
+            'kython': [
+                'py.typed',
+            ],
+        },
+    )
