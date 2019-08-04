8af7d75a166807c52a9491c6937c7d02869d7a9a
Dima Gerasimov <karlicoss@gmail.com>
2019-08-04 18:19:14 +0100

    add listify

--- kython/__init__.py
+++ kython/__init__.py
@@ -1,6 +1,42 @@
+from functools import wraps
+
 from .misc import *
 
 def json_loadf(fname: str):
     import json
     with open(fname, 'r') as fo:
         return json.load(fo)
+
+
+# https://stackoverflow.com/a/12377059/706389
+def listify(fn=None, wrapper=list):
+    """
+    A decorator which wraps a function's return value in ``list(...)``.
+
+    Useful when an algorithm can be expressed more cleanly as a generator but
+    the function should return an list.
+
+    Example::
+
+        >>> @listify
+        ... def get_lengths(iterable):
+        ...     for i in iterable:
+        ...         yield len(i)
+        >>> get_lengths(["spam", "eggs"])
+        [4, 4]
+        >>>
+        >>> @listify(wrapper=tuple)
+        ... def get_lengths_tuple(iterable):
+        ...     for i in iterable:
+        ...         yield len(i)
+        >>> get_lengths_tuple(["foo", "bar"])
+        (3, 3)
+    """
+    def listify_return(fn):
+        @wraps(fn)
+        def listify_helper(*args, **kw):
+            return wrapper(fn(*args, **kw))
+        return listify_helper
+    if fn is None:
+        return listify_return
+    return listify_return(fn)
